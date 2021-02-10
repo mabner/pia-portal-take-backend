aa9c10fbcd02830230690325e76b934390073272
Marcos Leite <30819867+mabner@users.noreply.github.com>
2021-02-10 01:34:46 -0300

    user creation test

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const { User } = require('./app/models');
 
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
+
+User.create({
+	name: 'Marcos',
+	email: 'marcos@abner.info.br',
+	password: '123456',
+});
 
 // Entry point for the app
 app.get('/', (req, res) => {
