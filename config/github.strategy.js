43fe1b59e434c49a17a0af235f2a2f2dab5a500b
Marcos Leite <30819867+mabner@users.noreply.github.com>
2021-04-22 05:06:35 -0300

    Adds the Github Strategy config file (ENV)

---
+++ config/github.strategy.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+import { use, serializeUser, deserializeUser } from 'passport';
+import { Strategy as GitHubStrategy } from 'passport-github2';
+
+use(
+	new GitHubStrategy(
+		{
+			clientID: process.env.GITHUB_CLIENT_ID,
+			clientSecret: process.env.GITHUB_CLIENT_SECRET,
+			callbackURL: process.env.GITHUB_CALLBACK_URL,
+		},
+		function (accessToken, refreshToken, profile, done) {
+			return done(undefined, profile);
+		}
+	)
+);
+
+serializeUser(function (user, done) {
+	done(undefined, user);
+});
+
+deserializeUser(function (user, done) {
+	done(undefined, user);
+});
